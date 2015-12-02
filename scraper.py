cf3947a5f0341f12e1a9d88693781bf1c5c82c6f
chuenll <chuenll@mcmaster.ca>
2015-12-01 23:33:56 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -16,6 +16,6 @@ def get_google_new_results( term, site, count ):
 
     return results
 
-items = get_google_new_results( 'Syrian+refugees', 'www.washingtonpost.com', 50 )
+items = get_google_new_results( 'Beirut+attack', 'www.washingtonpost.com', 50 )
 for i,e in enumerate(items):
     print '%d: %s' % (i+1,e,)
