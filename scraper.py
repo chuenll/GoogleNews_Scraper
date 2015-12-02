0ee5b86e97a93dbc0870ee7bd50fbebeb9af1ae7
chuenll <chuenll@mcmaster.ca>
2015-12-02 11:54:00 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -7,7 +7,7 @@ def get_google_new_results( term, site, count ):
     elements = obj.getElementsByTagName('title')[2:] # To get rid of unwanted title elements in XML doc    
     links = obj.getElementsByTagName('link')[2:]
     print links
-    for element in elements[:50]:
+    for element in elements[:count]:
         headline =  element.childNodes[0].data
         for link in links:
             url = link.childNodes[0].data.split('=')[-1]
@@ -16,6 +16,6 @@ def get_google_new_results( term, site, count ):
 
     return results
 
-items = get_google_new_results( 'Beirut+attack', 'www.nytimes.com', 20)
+items = get_google_new_results( 'Beirut+attack', 'www.globeandmail.ca', 20)
 for i,e in enumerate(items):
     print '%d: %s' % (i+1,e,)
